8a443e99268a529a84a190c18cc1a72c3b863ddd
Pekhota <pekhota.alex@gmail.com>
2018-10-22 18:12:48 +0300

    Fixed Alina comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if(process.argv.length < 4) {
+if (process.argv.length < 4) {
     throw new Error("You have to pass source json file and destination json file")
 }
 
@@ -11,131 +11,107 @@ const fs = require('fs');
 const originalJsonString = fs.readFileSync(source);
 
 // Convert it to js object
-let data = JSON.parse(originalJsonString);
+const data = JSON.parse(originalJsonString);
 
-let app = {};
 
-/**
- * Function to process array items in swagger specification
- * @param object
- * @returns {Array}
- */
-app.processArray = function (object) {
-    let toRet = [];
-    for (let i = 0; i < object.length; i++) {
-        let item = object[i];
-
-        toRet.push(app.traverse(item));
-    }
-    return toRet;
-};
-
-/**
- * Function to replace $ref key by proper object from #/ notation. Ex: #/definitions/PetModel
- * @param item
- * @returns {*}
- */
-app.processRef = function (item) {
-    let key2 = item['$ref'].replace('#/', '');
-    let key2split = key2.split('/');
-    let splitItems = key2split;
-    let nestedItem = data;
-    for (let i = 0; i < splitItems.length; i++) {
-        nestedItem = nestedItem[splitItems[i]];
+class App {
+    constructor(data) {
+        this.data = data;
     }
 
-    return app.traverse(nestedItem);
-};
-
-// start * https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge ******
-// we need deep recursive merge for 'allof' swagger key
-/**
- * Simple object check.
- * @param item
- * @returns {boolean}
- */
-app.isObject = function (item) {
-    return (item && typeof item === 'object' && !Array.isArray(item));
-};
-
-/**
- * Deep merge two objects.
- * @param target
- * @param sources
- */
-app.mergeDeep = function (target, ...sources) {
-    if (!sources.length) return target;
-    const source = sources.shift();
-
-    if (app.isObject(target) && app.isObject(source)) {
-        for (const key in source) {
-            // noinspection JSUnfilteredForInLoop
-            if (app.isObject(source[key])) {
-                if (!target[key]) Object.assign(target, {[key]: {}});
-                app.mergeDeep(target[key], source[key]);
-            } else {
-                Object.assign(target, {[key]: source[key]});
-            }
-        }
+    invoke() {
+        return this.traverse(this.data);
     }
 
-    return app.mergeDeep(target, ...sources);
-};
+    /**
+     * Function to process array items in swagger specification
+     * @param object
+     * @returns {Array}
+     */
+    processArray(object) {
+        return object.reduce((acc, currentValue) => acc.concat(this.traverse(currentValue)), []);
+    }
 
-// end *** https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge ******
+    /**
+     * Function to replace $ref key by proper object from #/ notation. Ex: #/definitions/PetModel
+     * @param item
+     * @returns {*}
+     */
+    processRef(item) {
+        return this.traverse(item['$ref'].replace('#/', '').split('/').reduce((acc, cur) => acc[cur], this.data));
+    };
 
-/**
- * Main recursive function to process swagger json
- * @param object
- * @returns {*}
- */
-app.traverse = function (object) {
-    if (typeof object === 'object') {
-        if (object.constructor === Array) {
-            return app.processArray(object)
-        } else {
+    // start * https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge ******
+    // we need deep recursive merge for 'allof' swagger key
+    /**
+     * Simple object check.
+     * @param item
+     * @returns {boolean}
+     */
+    isObject(item) {
+        return (item && typeof item === 'object' && !Array.isArray(item));
+    };
 
-            if (typeof object['$ref'] !== "undefined") {
-                return app.processRef(object);
-            } else {
-                if (typeof object['allOf'] !== "undefined") {
-                    let merged = {};
-                    for (let i = 0; i < object['allOf'].length; i++) {
-                        let item = object['allOf'][i];
-                        let processedItem = app.traverse(item);
-                        merged = app.mergeDeep(merged, processedItem);
-                    }
+    /**
+     * Deep merge two objects.
+     * @param target
+     * @param sources
+     */
+    mergeDeep(target, ...sources) {
+        if (!sources.length) return target;
+        const source = sources.shift();
 
-                    return merged;
+        if (this.isObject(target) && this.isObject(source)) {
+            for (const key in source) {
+                // noinspection JSUnfilteredForInLoop
+                if (this.isObject(source[key])) {
+                    if (!target[key]) Object.assign(target, {[key]: {}});
+                    this.mergeDeep(target[key], source[key]);
                 } else {
-                    let toRet = {};
-                    for (let key in object) {
-                        let item = object[key];
-
-                        if (typeof item === 'object') {
-                            if (item.constructor === Array) {
-                                toRet[key] = app.processArray(item);
-                            } else {
-                                if (typeof item['$ref'] === "undefined") {
-                                    toRet[key] = app.traverse(item);
-                                } else {
-                                    toRet[key] = app.traverse(item);
-                                }
-                            }
-                        } else {
-                            toRet[key] = item;
-                        }
-
-                    }
-                    return toRet;
+                    Object.assign(target, {[key]: source[key]});
                 }
             }
         }
-    } else {
-        return object;
-    }
-};
 
-let res = app.traverse(data);
+        return this.mergeDeep(target, ...sources);
+    };
+
+    // end *** https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge ******
+
+    /**
+     * Main recursive function to process swagger json
+     * @param object
+     * @returns {*}
+     */
+    traverse(object) {
+
+        if(typeof object !== 'object') return object;
+
+        if (Array.isArray(object)) return this.processArray(object);
+
+
+        if (typeof object['$ref'] !== "undefined") return this.processRef(object);
+
+
+        if (typeof object['allOf'] !== "undefined") return object['allOf'].reduce((acc, cur) => this.mergeDeep(acc, this.traverse(cur)), {});
+
+
+        return Object.entries(object).reduce((acc, cur) => {
+            const item = cur[1];
+            const key = cur[0];
+
+            if (typeof item === 'object') {
+                return Object.assign(acc, {[key]: Array.isArray(item) ? this.processArray(item) : this.traverse(item)});
+            } else {
+                return Object.assign(acc, {[key]: item});
+            }
+        }, {});
+    };
+}
+
+
+const app = new App(data);
+
+const res = app.invoke();
 
 fs.writeFileSync(destination, JSON.stringify(res));
